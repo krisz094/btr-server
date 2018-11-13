941c0407bdeac7a6f91a67e6f4c5557a5000d90a
Kulcsár Kristóf <rsrlz94@gmail.com>
2018-11-13 22:22:10 +0100

    fix bad request logging

--- api/responses/badRequest.js
+++ api/responses/badRequest.js
@@ -2,6 +2,6 @@ module.exports = function badRequest(err) {
   var req = this.req;
   var res = this.res;
 
-  sails.log(err);
+  sails.log(err.toString());
   return res.status(400).json({ errorMessage: err.toString() });
 }
